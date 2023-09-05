81e616b711f47f478ee54529ba7e3d5936c2bad7
amirnilofari <amirnilofari@gmail.com>
2023-09-06 00:14:56 +0330

    create function for some operation

--- main.go
+++ main.go
@@ -6,34 +6,25 @@ import (
 	"strconv"
 )
 
+var balance int
+
 func main() {
+	balance = 2500
 	fmt.Println("Welcome to the ATM bank")
 	fmt.Println("Insert the card into the ATM and enter your four-digit password:")
 
 	var password int
-	fmt.Scan(&password)
-	if len(strconv.Itoa(password)) != 4 {
-		fmt.Println("Something wrong.Please try again!")
-		os.Exit(0)
-	}
-	fmt.Println("Your password is: ", password)
-
-	fmt.Println("Now you can select the desired number and perform the desired operation")
-	fmt.Println("[1] - Your balance account")
-	fmt.Println("[2] - Withdraw from your account")
-	fmt.Println("[3] - Deposit to your account")
-	fmt.Println("[4] - Transfer from your account")
+	CheckPassword(password)
 
 	var operation int
 	fmt.Scan(&operation)
+
 	if operation == 1 {
-		fmt.Println("Your balance account is: ", "1500$")
+		Balance()
 	}
 	if operation == 2 {
-		fmt.Println("Enter the withdrawal amount:")
-		var withdrawal int
-		fmt.Scan(&withdrawal)
-		fmt.Println("The amount of " + strconv.Itoa(withdrawal) + "$ was deducted from your account")
+		var amount int
+		Withdrawal(amount)
 	}
 	if operation == 3 {
 		fmt.Println("Deposit")
@@ -48,4 +39,35 @@ func main() {
 	// if operations[1] == "Withdraw" {
 	// 	fmt.Println("Withdraw")
 	// }
+}
+
+func CheckPassword(pass int) {
+	fmt.Scan(&pass)
+	if len(strconv.Itoa(pass)) != 4 {
+		fmt.Println("Something wrong.Please try again!")
+		os.Exit(0)
+	}
+
+	fmt.Println("Now you can select the desired number and perform the desired operation")
+	fmt.Println("[1] - Your balance account")
+	fmt.Println("[2] - Withdraw from your account")
+	fmt.Println("[3] - Deposit to your account")
+	fmt.Println("[4] - Transfer from your account")
+}
+
+func Balance() {
+	fmt.Println("Your account balance is $" + strconv.Itoa(balance))
+}
+
+func Withdrawal(amount int) {
+	fmt.Println("Enter the withdrawal amount:")
+	fmt.Scan(&amount)
+
+	if amount > balance {
+		fmt.Println("The requested amount exceeds your account balance!")
+	} else {
+		fmt.Println("The amount of $" + strconv.Itoa(amount) + " was deducted from your account")
+		fmt.Println("Your account balance is $" + strconv.Itoa(balance-amount))
+	}
+
 }
